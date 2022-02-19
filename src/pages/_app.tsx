1fbd9b9b0915831cfde567d8b7a8f3273c32e0aa
Renan Hilario <rvahilario@gmail.com>
2022-02-19 15:20:55 -0300

    style(🎨 ): formating files with new settings

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,5 +2,5 @@ import { AppProps } from 'next/app';
 import '@/styles/global.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+	return <Component {...pageProps} />;
 }
