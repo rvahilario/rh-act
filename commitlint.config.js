4ddae801b623c4190fe3e0c0c4789930b4b358a8
Renan Hilario <rvahilario@gmail.com>
2022-02-21 21:29:43 -0300

    feat: :star2: testing

---
+++ commitlint.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+	extends: ['@commitlint/config-conventional'],
+	rules: {
+		'type-enum': [
+			2,
+			'always',
+			[
+				'feat',
+				// 'fix',
+				// 'style',
+				// 'refact',
+				// 'test',
+				// 'config',
+				// 'del',
+				// 'dep-add',
+				// 'dep-rm',
+				// 'gitingore',
+				// 'docs',
+				// 'perf',
+				// 'start',
+				// 'moving',
+				// 'assets',
+			],
+		],
+	},
+	parserPreset: {
+		parserOpts: {
+			headerCorrespondence: ['type', 'scope', 'subject', 'ticket'],
+		},
+	},
+};
